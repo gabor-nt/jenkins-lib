ced13f3ddfc8ba792cc16d4732394bcdd58033fe
Gábor Takács <gabor.takacs@semcon.com>
2018-11-20 10:28:35 +0100

    3

--- vars/serviceMRBuildPipeline.groovy
+++ vars/serviceMRBuildPipeline.groovy
@@ -1,5 +1,10 @@
-import nexus
-
 def call(body) {
-    nexus.delete("sws","1.4.1")
+    delete("sws","1.4.1")
+}
+
+def delete(String artifact, String version) {
+    def connection = new URL("https://nexus.tools.tools178.k8syard.com/service/siesta/rest/beta/search?name=${artifact}&version=${version}").openConnection()
+    connection.setRequestMethod("GET")
+    connection.setRequestProperty("Authorization", "Basic ZGRhZG1pbjpBbHRlcm5hdGl2ZWx5Tm9uc2Vuc2U=")
+    println(connection.getInputStream().getText())
 }
